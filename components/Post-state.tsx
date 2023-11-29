6be0c109f0890ff8fe659e5eac2e26f2c631bd16
Artem Shakun <artem.shakun.91@gmail.com>
2023-11-29 16:35:38 +0200

    client-component

---
+++ components/Post-state.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Link from 'next/link';
+import { usePosts } from '@/store';
+import { shallow } from 'zustand/shallow';
+import { useEffect } from 'react';
+import { PostSearch } from '@/components/PostSearch';
+
+export const PostsState = () => {
+  const [posts, loading, getAllPosts] = usePosts(
+    state => [state.posts, state.loading, state.getAllPosts],
+    shallow,
+  );
+
+  useEffect(() => {
+    getAllPosts();
+  }, [getAllPosts]);
+
+  return loading ? (
+    <h3>Loading ...</h3>
+  ) : (
+    <ul>
+      {posts.map((post: any) => (
+        <li key={post.id}>
+          <Link href={`/blog/${post.id}`}>{post.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
